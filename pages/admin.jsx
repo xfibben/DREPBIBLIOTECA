00c0a7a966e51b5dd773c471a2e7c385e0021731
xfibben <87906057+xfibben@users.noreply.github.com>
2022-12-22 11:45:46 -0500

    fixing admin view

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,36 +1,48 @@
-import { useSession, signOut } from "next-auth/react";
-import { Router, useRouter } from "next/router";
+import {  useRouter } from "next/router";
 import NavbarAdmin from './components/navbar'
 import {useUser} from "@auth0/nextjs-auth0/client";
+import Sidebar from "./components/sidebar";
 
 export default function Admin({ books }) {
 
   const router = useRouter();
   const { user,status,isLoading } = useUser();
+
   if (isLoading) {
     return <p>Loading....</p>;
 
   }
-  if (user) {
+  if (user.nickname=="xfibben") {
 
 
     return (
-      <div className="bg-gray-700 h-screen">
+      <div className="bg-gray-700 h-full ">
       <NavbarAdmin/>
+        <div className={'lg:flex my-10'}>
+          <Sidebar/>
 
-        <div className={'grid grid-cols-5 gap-5'}>
-          {books.map(book=>(
-              <div className={'m-5'} key={book._id} onClick={()=>router.push(`/libros/${book._id}/edit`)}>
-                <h1 className={'text-2xl text-white flex justify-center'}>{book.title}</h1>
-                <img className='w-full h-full' src={book.image}></img>
-              </div>
-          ))}
+          <div className={'grid  lg:w-3/4'}>
+            {books.map(book=>(
+                <div className={'m-5 h-10   w-full'} key={book._id} >
+                  <div className={'w-full flex'}>
+                    <h1 className={'text-2xl text-white flex justify-center'}>{book.title}</h1>
+                    <img className='w-10 h-10' src={book.image}/>
+                    <button className={'rounded-2xl mx-auto  mr-10 w-20 bg-green-500 '} onClick={()=>router.push(`libros/${book._id}/edit`)}>Editar</button>
+                  </div>
+                  <div className={'h-1 bg-white w-full mx-2'}></div>
 
+
+                </div>
+
+
+            ))}
+
+          </div>
         </div>
       </div>
     );
   } else {
-    router.push('/signin')
+    router.push('/')
 
   }
 }
