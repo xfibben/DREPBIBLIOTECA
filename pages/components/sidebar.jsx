b6618e8a1abded1bfcc43dd804861c06413118ae
zarufzaber <112907549+zarufzaber@users.noreply.github.com>
2023-01-02 15:12:52 -0500

    Sidebar_2
    Hi there, this is my part!!!

--- pages/components/sidebar.jsx
+++ pages/components/sidebar.jsx
@@ -6,13 +6,20 @@ export default function Sidebar(){
             <h1 className={'text-4xl'}>Categorias</h1>
             <div className={'bg-black h-1 w-full my-1 '}></div>
             <div className={'f-full grid grid-cols-2 gap-2 lg:grid-cols-1'}>
-                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>Historia</h1>
-                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>Matematicas</h1>
-                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>Ciencia</h1>
-                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>Filosofia</h1>
-                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>Geografia</h1>
-                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>Quimica</h1>
-                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>Fisica</h1>
+                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>
+                    <a href="http://localhost:8080/libros/categorias/historia">Historia</a></h1>
+                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>
+                    <a href="http://localhost:8080/libros/categorias/matematica">Matematicas</a></h1>
+                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>
+                    <a href="http://localhost:8080/libros/categorias/ciencia">Ciencia</a></h1>
+                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>
+                    <a href="http://localhost:8080/libros/categorias/filosofia">Filosofia</a></h1>
+                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>
+                    <a href="http://localhost:8080/libros/categorias/geografia">Geografia</a></h1>
+                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>
+                    <a href="http://localhost:8080/libros/categorias/quimica">Quimica</a></h1>
+                <h1 className={'transition ease-in-out my-3  hover:-translate-y-1 hover:scale-400  duration-300'}>
+                    <a href="http://localhost:8080/libros/categorias/fisica">Fisica</a></h1>
 
             </div>
 
